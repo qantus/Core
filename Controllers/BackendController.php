3935705ddda06850b3d8b7a0bcace29dd6cacfc2
Falaleev Maxim <max@studio107.ru>
2014-09-11 11:40:18 +0400

    wtf

--- Controllers/BackendController.php
+++ Controllers/BackendController.php
@@ -2,11 +2,15 @@
 
 namespace Modules\Core\Controllers;
 
+use Mindy\Base\ApplicationList;
 use Mindy\Base\Mindy;
+use Mindy\Http\Request;
 use Modules\Core\CoreModule;
 
 class BackendController extends CoreController
 {
+    use ApplicationList;
+
     public function accessRules()
     {
         return [
@@ -31,15 +35,21 @@ class BackendController extends CoreController
         return $filters;
     }
 
-    public function __construct($id, $module = null)
+    public function __construct($id, $module = null, Request $request)
     {
-        parent::__construct($id, $module);
+        parent::__construct($id, $module, $request);
 
         $this->pageTitle = CoreModule::t('Control panel');
 
         $user = Mindy::app()->user;
         if ($user === null || ($user && ($user->is_superuser === false || $user->is_staff === false))) {
-            $this->redirect('admin.login');
+            $this->r->redirect('admin.login');
         }
+    }
+
+    public function render($view, array $data = [])
+    {
+        $data['apps'] = $this->getApplications();
+        return parent::render($view, $data);
     }
 }
