b2282695bd1c112bc32034f428b1296fd4c7e285
antonokulov <qantus@mail.ru>
2014-10-07 05:49:52 +0400

    Humanize price

--- Components/Humanize.php
+++ Components/Humanize.php
@@ -47,6 +47,50 @@ class Humanize
         return $ending;
     }
 
+    /**
+     * Рубли из строки в число
+     * @param $num
+     * @return string
+     */
+    function numToStr($num) {
+        $nul='ноль';
+        $ten=array(
+            array('','один','два','три','четыре','пять','шесть','семь', 'восемь','девять'),
+            array('','одна','две','три','четыре','пять','шесть','семь', 'восемь','девять'),
+        );
+        $a20=array('десять','одиннадцать','двенадцать','тринадцать','четырнадцать' ,'пятнадцать','шестнадцать','семнадцать','восемнадцать','девятнадцать');
+        $tens=array(2=>'двадцать','тридцать','сорок','пятьдесят','шестьдесят','семьдесят' ,'восемьдесят','девяносто');
+        $hundred=array('','сто','двести','триста','четыреста','пятьсот','шестьсот', 'семьсот','восемьсот','девятьсот');
+        $unit=array( // Units
+            array('копейка' ,'копейки' ,'копеек',	 1),
+            array('рубль'   ,'рубля'   ,'рублей'    ,0),
+            array('тысяча'  ,'тысячи'  ,'тысяч'     ,1),
+            array('миллион' ,'миллиона','миллионов' ,0),
+            array('миллиард','милиарда','миллиардов',0),
+        );
+        //
+        list($rub,$kop) = explode('.',sprintf("%015.2f", floatval($num)));
+        $out = array();
+        if (intval($rub)>0) {
+            foreach(str_split($rub,3) as $uk=>$v) { // by 3 symbols
+                if (!intval($v)) continue;
+                $uk = sizeof($unit)-$uk-1; // unit key
+                $gender = $unit[$uk][3];
+                list($i1,$i2,$i3) = array_map('intval',str_split($v,1));
+                // mega-logic
+                $out[] = $hundred[$i1]; # 1xx-9xx
+                if ($i2>1) $out[]= $tens[$i2].' '.$ten[$gender][$i3]; # 20-99
+                else $out[]= $i2>0 ? $a20[$i3] : $ten[$gender][$i3]; # 10-19 | 1-9
+                // units without rub & kop
+                if ($uk>1) $out[]= self::getNumEnding($v,[$unit[$uk][0],$unit[$uk][1],$unit[$uk][2]]);
+            } //foreach
+        }
+        else $out[] = $nul;
+        $out[] = self::getNumEnding(intval($rub), [$unit[1][0],$unit[1][1],$unit[1][2]]); // rub
+        $out[] = $kop.' '.self::getNumEnding($kop, [$unit[0][0],$unit[0][1],$unit[0][2]]); // kop
+        return trim(preg_replace('/ {2,}/', ' ', join(' ',$out)));
+    }
+
     public static function humanizeDate(DateTime $date, $dateFormat = 'd.m.Y')
     {
         $now = new DateTime();
