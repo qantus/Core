ee457c37482dcdcb2985bff3cf9749e960f88708
antonokulov <qantus@mail.ru>
2014-12-02 11:42:16 +0300

    Validation messages

--- messages/ru/validation.php
+++ messages/ru/validation.php
@@ -4,6 +4,7 @@ return array(
     'Value cannot be empty' => 'Поле не может быть пустым',
     'Is not a valid email address' => 'Некорректный адрес электронной почты',
     '"{name}" cannot be empty' => '"{name}" не может быть пустым',
+    '{name} cannot be empty' => '{name} не может быть пустым',
     'Is not a valid file type {type}. Types allowed: {allowed}' => 'Некорректный тип файла "{type}". Разрешенные типы: {allowed}',
     'Maximum length is {length}' => 'Масимальная длина - {length} символов',
     'Minimal length is {length}' => 'Минимальная длина - {length} символов',
