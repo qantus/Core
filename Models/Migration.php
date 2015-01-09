4b385078c7806c1dd5fb60d63e3c972b1ba6d45d
Falaleev Maxim <max@studio107.ru>
2015-01-09 15:03:13 +0300

    Add migration command

---
+++ Models/Migration.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * All rights reserved.
+ *
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 08/01/15 18:32
+ */
+
+namespace Modules\Core\Models;
+
+use Mindy\Orm\Fields\CharField;
+use Mindy\Orm\Fields\IntField;
+use Mindy\Orm\Model;
+
+/**
+ * Class Core
+ * @package Modules\Core
+ * @method static \Modules\Core\Models\MigrationManager objects($instance = null)
+ */
+class Migration extends Model
+{
+    public static function getFields()
+    {
+        return [
+            'module' => [
+                'class' => CharField::className(),
+            ],
+            'model' => [
+                'class' => CharField::className(),
+            ],
+            'timestamp' => [
+                'class' => IntField::className(),
+            ],
+        ];
+    }
+
+    public static function objectsManager($instance = null)
+    {
+        $className = get_called_class();
+        return new MigrationManager($instance ? $instance : new $className);
+    }
+}
