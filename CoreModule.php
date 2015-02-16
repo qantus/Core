ddc9775e25ccd5e7b3032ea373b5de6221d72ad8
Falaleev Maxim <max@studio107.ru>
2015-02-16 18:49:22 +0300

    Add locale helper

--- CoreModule.php
+++ CoreModule.php
@@ -42,6 +42,9 @@ class CoreModule extends Module
         $tpl->addHelper('debug_panel', ['\Modules\Core\Components\DebugPanel', 'render']);
         $tpl->addHelper('param', ['\Modules\Core\Components\ParamsHelper', 'get']);
         $tpl->addHelper('humanizeDateTime', ['\Modules\Core\Components\Humanize', 'humanizeDateTime']);
+        $tpl->addHelper('locale', function() use ($app) {
+            return $app->locale;
+        });
         $tpl->addHelper('humanizeSize', ['\Modules\Core\Components\Humanize', 'humanizeSize']);
         $tpl->addHelper('humanizePrice', ['\Modules\Core\Components\Humanize', 'numToStr']);
         $tpl->addHelper('limit', ['\Mindy\Helper\Text', 'limit']);
