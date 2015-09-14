ae683f2ec12bfb8673a84d1d7aa571d6b1ba335f
Max107 <max@studio107.ru>
2015-09-14 22:22:02 +0300

    remove unused templates

--- Middleware/CorsMiddleware.php
+++ Middleware/CorsMiddleware.php
@@ -9,18 +9,22 @@ use Mindy\Middleware\Middleware;
 class CorsMiddleware extends Middleware
 {
     /**
-     * @var array
+     * @var string
      */
-    public $methods = [];
+    public $origin = '*';
     /**
      * @var array
      */
-    public $headers = [];
+    public $methods = ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
+    /**
+     * @var array
+     */
+    public $headers = ['X-Auth-Token', 'X-Requested-With', 'X-CSRFToken', 'Content-Type'];
 
     public function processRequest(Request $request)
     {
         if (Console::isCli() == false) {
-            $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*';
+            $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : $this->origin;
             header('Access-Control-Allow-Origin: ' . $origin);
             header('Access-Control-Allow-Methods: ' . (is_string($this->methods) ? $this->methods : implode($this->methods, ', ')));
             header('Access-Control-Allow-Headers: ' . (is_string($this->headers) ? $this->headers : implode($this->headers, ', ')));
